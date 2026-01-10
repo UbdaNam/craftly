f74cd302dda7c9508073e75d8c6e186025aaf78b
UbdaNam <abdugreat2@gmail.com>
2026-01-10 14:22:11 +0300

    Create placeholder pages for inbox and teams routes

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,5 +1,5 @@
 const Content = () => {
-  return <div>Content</div>;
+  return <div className="m-2 px-2">Content</div>;
 };
 
 export default Content;
---
+++ src/pages/Inbox.tsx
@@ -0,0 +1,5 @@
+const Inbox = () => {
+  return <div className="m-2 px-2">Inbox</div>;
+};
+
+export default Inbox;
